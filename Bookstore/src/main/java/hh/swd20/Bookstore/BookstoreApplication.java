4b86ce5e0e7d27cbb783bd2486dffff764ae6446
heuui <henni.haavistola@gmail.com>
2022-02-14 11:17:28 +0200

    Adding JPA and database

--- Bookstore/src/main/java/hh/swd20/Bookstore/BookstoreApplication.java
+++ Bookstore/src/main/java/hh/swd20/Bookstore/BookstoreApplication.java
@@ -1,13 +1,37 @@
 package hh.swd20.Bookstore;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import hh.swd20.Bookstore.domain.Book;
+import hh.swd20.Bookstore.domain.BookRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
+	private static final Logger log = LoggerFactory.getLogger(BookstoreApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner bookDemo(BookRepository repository) {
+		return (args) -> {
+			log.info("Save a few books"); // save to database
+			repository.save(new Book("The Picture of Dorian Gray", "Oscar Wilde", 1891, "978-951-1-25774-5", 9.99));
+			repository.save(new Book("Lord of the Flies", "William Golding", 1960, "978-951-1-27428-5", 9.90));
+			repository.save(new Book("The Story of The Human Body", "Daniel Lieberman", 2013, "978-0-141-39995-9", 12.90));
+
+			log.info("fetch all books"); // get from database
+			for (Book book : repository.findAll()) {
+				log.info(book.toString());
+			}
+		};
 	}
 
 }
--- Bookstore/src/main/java/hh/swd20/Bookstore/domain/Book.java
+++ Bookstore/src/main/java/hh/swd20/Bookstore/domain/Book.java
@@ -1,7 +1,16 @@
 package hh.swd20.Bookstore.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Book {
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
 	private String title;
 	private String author;
 	private int year;
@@ -26,6 +35,14 @@ public class Book {
 		this.price = price;
 	}
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
 	public String getTitle() {
 		return title;
 	}
@@ -68,7 +85,7 @@ public class Book {
 
 	@Override
 	public String toString() {
-		return "Title: " + title + ", Author: " + author + ", Year: " + year + ", ISBN: " + isbn + ", Price: " + price
+		return "Id: " + id + "Title: " + title + ", Author: " + author + ", Year: " + year + ", ISBN: " + isbn + ", Price: " + price
 				+ ". ";
 	}
 	
