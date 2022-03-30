62820de3fdaa695dddc12f6fdc23bef0a3e2422b
heuui <henni.haavistola@gmail.com>
2022-03-30 16:13:42 +0300

    adding JPA repository tests

--- Bookstore/src/test/java/hh/swd20/Bookstore/BookstoreApplicationTests.java
+++ Bookstore/src/test/java/hh/swd20/Bookstore/BookstoreApplicationTests.java
@@ -1,13 +1,24 @@
 package hh.swd20.Bookstore;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import hh.swd20.Bookstore.web.BookController;
+
+@ExtendWith(SpringExtension.class)   // JUnit5 eli Jupiter
 @SpringBootTest
 class BookstoreApplicationTests {
 
+	@Autowired
+	private BookController bookController;
+	
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		assertThat(bookController).isNotNull();
 	}
 
 }
